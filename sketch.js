8e59fe16922875ecb2e52a948cf23cd7950aae37
Deepu300 <159274374+Deepu300@users.noreply.github.com>
2025-02-05 17:41:01 -0500

    HW Implementation

--- sketch.js
+++ sketch.js
@@ -1,15 +1,70 @@
-let mCamera;
+let grid = [];
+let cols, rows;
+let size = 10;
+
+let handPose;
+let video;
+let hands = [];
+let options = { flipped: true };
 
 function preload() {
-  mCamera = createCapture(VIDEO, { flipped: true });
-  mCamera.hide();
+  handPose = ml5.handPose(options);
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(640, 480);
+  video = createCapture(VIDEO, { flipped: true });
+  video.size(640, 480);
+  video.hide();
+  handPose.detectStart(video, gotHands);
+
+  cols = floor(width / size);
+  rows = floor(height / size);
+  for (let i = 0; i < cols; i++) {
+    grid[i] = [];
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = 0;
+    }
+  }
 }
 
 function draw() {
-  background(180, 200, 255);
-  image(mCamera, 0, 0);
+  background(0);
+
+  image(video, 0, 0, width, height);
+
+  for (let i = 0; i < hands.length; i++) {
+    let hand = hands[i];
+    let indexFinger = hand.keypoints[8]; // Index finger tip
+    addCoins(indexFinger.x, indexFinger.y);
+  }
+
+  drawCoins(); 
+}
+
+function drawCoins() {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      if (grid[i][j] > 0) {
+        noStroke();
+        fill(255, 223, 0); // Gold color
+        ellipse(i * size + size / 2, j * size + size / 2, size, size);
+        fill(0);
+        rectMode(CENTER);
+        rect(i * size + size / 2, j * size + size / 2, size / 3, size / 3);
+      }
+    }
+  }
+}
+
+function addCoins(fingerX, fingerY) {
+  let x = floor(fingerX / size);
+  let y = floor(fingerY / size);
+  x = constrain(x, 0, cols - 1);
+  y = constrain(y, 0, rows - 1);
+  grid[x][y] = 1; // Place coin at detected finger position
+}
+
+function gotHands(results) {
+  hands = results;
 }
